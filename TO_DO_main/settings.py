12beee8fe150ecaf14727b8de3a0c0a01981bfb7
Pavan Kiran <pavankiran26082003@gmail.com>
2025-09-10 16:38:23 +0530

    guricon

--- TO_DO_main/settings.py
+++ TO_DO_main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8w5e7s)m7$9u$%i19nzo!$pjc+!ymy@!mu)(smnoi_w5yy#zy*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
